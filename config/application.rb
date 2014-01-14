8227c1d3508516fb88908d7dd7775fc4d805ac6a
Vanessa Pai <fanchen.pai@gmail.com>
2014-01-14 08:50:00 -0800

    supress I18n validation deprecation warning

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,10 @@ Bundler.require(:default, Rails.env)
 
 module Myflix
   class Application < Rails::Application
+    # Supress the I18n validation deprecation warning. See this post for detail:
+    # http://stackoverflow.com/questions/20361428/rails-i18n-validation-deprecation-warning
+    I18n.enforce_available_locales = true
+
     config.encoding = "utf-8"
     config.filter_parameters += [:password]
     config.active_support.escape_html_entities_in_json = true
