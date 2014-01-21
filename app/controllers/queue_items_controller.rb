7572050cf69b383705309a017e3e47234c416ec4
Vanessa Pai <fanchen.pai@gmail.com>
2014-01-20 19:52:01 -0800

    refactor queue items controller to make it thinner

--- app/controllers/queue_items_controller.rb
+++ app/controllers/queue_items_controller.rb
@@ -12,62 +12,47 @@ class QueueItemsController < ApplicationController
   end
 
   def update_queue
-    begin
-      ActiveRecord::Base.transaction do
-        params[:queue_items].each do |item_param|
-          update_queue_item_position(item_param['id'], item_param['position'])
-        end
-        normalize_position_number
-      end
-    rescue ActiveRecord::RecordInvalid
-      flash[:error] = 'Invalid postion number format'
-    rescue Exception => e
-      flash[:error] = e.message
-    end
+    update_queue_item_positions
     redirect_to my_queue_path
   end
 
   def destroy
-    ActiveRecord::Base.transaction do
-      queue_item = QueueItem.find(params[:id])
-      queue_item.destroy if current_user.queue_items.include?(queue_item)
-      normalize_position_number
-    end
+    delete_queue_item
     redirect_to my_queue_path
   end
 
   private
 
   def queue_video(video)
-    if current_user_queued_video?(video)
+    if current_user.queued_video?(video)
       flash[:error] = "This video is already in [My Queue]."
     else
-      QueueItem.create(video: video, user:current_user, position: new_queue_item_position)
+      QueueItem.create(video: video, user:current_user, position: current_user.new_queue_item_position)
       flash[:notice] = "Video added to [My Queue]."
     end
   end
 
-  def new_queue_item_position
-    current_user.queue_items.count + 1
-  end
-
-  def current_user_queued_video?(video)
-    current_user.queue_items.map(&:video).include?(video)
-  end
-
-  def normalize_position_number
-    current_user.queue_items.each_with_index do |item, index|
-      item.update_attribute(:position, index+1)
+  def update_queue_item_positions
+    begin
+      ActiveRecord::Base.transaction do
+        params[:queue_items].each do |item_param|
+          current_user.update_queue_item_position(item_param['id'], item_param['position'])
+        end
+        current_user.normalize_position_number
+      end
+    rescue ActiveRecord::RecordInvalid
+      flash[:error] = 'Invalid postion number format'
+    rescue Exception => e
+      flash[:error] = e.message
     end
   end
 
-  def update_queue_item_position(item_id, new_position)
-    item = QueueItem.find(item_id)
-    if current_user.queue_items.include?(item)
-      item.position = new_position
-      item.save!
-    else
-      raise 'You can only re-order videos in your queue.'
+  def delete_queue_item
+    ActiveRecord::Base.transaction do
+      queue_item = QueueItem.find(params[:id])
+      queue_item.destroy if current_user.queue_items_include?(queue_item)
+      current_user.normalize_position_number
     end
   end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,32 @@ class User < ActiveRecord::Base
   validates :password_digest, presence: true
   validates :password, length: { minimum: 3 }
   has_secure_password
+
+  def queued_video?(video)
+    queue_items.map(&:video).include?(video)
+  end
+
+  def queue_items_include?(queue_item)
+    queue_items.include?(queue_item)
+  end
+
+  def normalize_position_number
+    queue_items.each_with_index do |item, index|
+      item.update_attribute(:position, index+1)
+    end
+  end
+
+  def update_queue_item_position(item_id, new_position)
+    item = QueueItem.find(item_id)
+    if queue_items.include?(item)
+      item.position = new_position
+      item.save!
+    else
+      raise 'You can only re-order videos in your queue.'
+    end
+  end
+
+  def new_queue_item_position
+    queue_items.count + 1
+  end
 end
